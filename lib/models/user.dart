fc578d865bb9ab28ea849881bbc4e1bb0d63f2e1
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-23 05:37:55 +0300

    user.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+// ignore: file_names
+
+class Userclass {
+  late String username;
+  late String password;
+  late String email;
+  late String phoneNumber;
+
+  Userclass({
+    required this.username,
+    required this.password,
+    required this.email,
+    required this.phoneNumber,
+  });
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
+
+    print("Email: $email, Password: $password");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            const SizedBox(height: 12),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: _login, child: const Text("Login")),
+          ],
+        ),
+      ),
+    );
+  }
+}
