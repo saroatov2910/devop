bc1e5767f4b1112f8aacfbe6fbfa603086a92ca1
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-24 13:35:58 +0300

    add the Map<String, dynamic> toJson

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,12 +3,13 @@
 // it contains common properties like username, password, email, and phoneNumber
 
 abstract class User {
-  // common properties for all users
   late String username;
   late String password;
   late String email;
   late String phoneNumber;
   late String userId;
+  late DateTime createdAt;
+  bool isVerified;
 
   User({
     required this.username,
@@ -16,5 +17,21 @@ abstract class User {
     required this.email,
     required this.phoneNumber,
     required this.userId,
-  });
+    DateTime? createdAt,
+    this.isVerified = false,
+  }) : createdAt = createdAt ?? DateTime.now();
+
+  // method to convert user object to a map for easier storage in Firestore
+  // especially useful when registering a new user
+  // and you want to save their details in the database
+  Map<String, dynamic> toJson() {
+    return {
+      'username': username,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'userId': userId,
+      'createdAt': createdAt.toIso8601String(),
+      'isVerified': isVerified,
+    };
+  }
 }
