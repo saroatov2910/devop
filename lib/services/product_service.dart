71a3a96114b7f60c4a489abdbeed131082efc7b5
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-26 01:13:29 +0300

    The function you sent, getProducts(), creates a live connection to Firebase.

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -1,4 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:devop/models/products/product.dart';
 import '../services/product_service.dart';
 
-class ProductService {}
+class ProductService {
+  Stream<List<Product>> getProducts() {
+    return productsCollection.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return Product(
+          id: doc.id,
+          nameProduct: doc['nameProduct'],
+          description: doc['description'],
+        );
+      }).toList();
+    });
+  }
+}
