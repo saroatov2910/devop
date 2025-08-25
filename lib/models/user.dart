9164afbd9dc5e973119d86d8402412a6a3e0fde9
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-25 10:25:11 +0300

    Add NewUser data model class
    Created a simple NewUser class to store user information including username, password, email, phone number, and user ID. The class includes a constructor for initialization and can be extended later to support JSON serialization for database storage.

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,37 +1,15 @@
-// lib/models/user.dart
-// this is the base user class for all users in the app
-// it contains common properties like username, password, email, and phoneNumber
+class NewUser {
+  String username;
+  String password;
+  String email;
+  String phoneNumber;
+  String userId;
 
-abstract class User {
-  late String username;
-  late String password;
-  late String email;
-  late String phoneNumber;
-  late String userId;
-  late DateTime createdAt;
-  bool isVerified;
-
-  User({
+  NewUser({
     required this.username,
     required this.password,
     required this.email,
     required this.phoneNumber,
     required this.userId,
-    DateTime? createdAt,
-    this.isVerified = false,
-  }) : createdAt = createdAt ?? DateTime.now();
-
-  // method to convert user object to a map for easier storage in Firestore
-  // especially useful when registering a new user
-  // and you want to save their details in the database
-  Map<String, dynamic> toJson() {
-    return {
-      'username': username,
-      'email': email,
-      'phoneNumber': phoneNumber,
-      'userId': userId,
-      'createdAt': createdAt.toIso8601String(),
-      'isVerified': isVerified,
-    };
-  }
+  });
 }
