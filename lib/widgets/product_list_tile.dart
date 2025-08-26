dad04ed14300c4cedb32e28ef83462607f33ab00
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-26 12:42:16 +0300

    Create ProductListTile with required parameters
    - This commit adds a constructor to the ProductListTile widget.
    - The constructor now requires 'product' and 'onTap' parameters, ensuring the widget is properly initialized and reusable.
    - It formalizes the contract for the widget, making its usage clear and explicit.

--- lib/widgets/product_list_tile.dart
+++ lib/widgets/product_list_tile.dart
@@ -7,4 +7,11 @@ import 'package:your_app_name/models/products/product.dart';
 class ProductListTile extends StatefulWidget {
   final Product product;
   final VoidCallback;
+
+
+  const ProductListTile ({
+    super.key,
+    required this.product,
+    required this.onTap ;
+  });
 }
