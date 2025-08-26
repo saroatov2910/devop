f3c6b6bf16424dd5865df62b974bdfabef8d1966
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-26 12:37:13 +0300

    Implement ProductListTile widget

--- lib/widgets/product_list_tile.dart
+++ lib/widgets/product_list_tile.dart
@@ -1,4 +1,10 @@
+import 'dart:ffi';
+
+import 'package:devop/models/products/product.dart';
 import 'package:flutter/material.dart';
 import 'package:your_app_name/models/products/product.dart';
 
-class ProductListTile extends StatefulWidget {}
+class ProductListTile extends StatefulWidget {
+  final Product product;
+  final VoidCallback;
+}
