528d6c245f391455f514b0f5cc95627805032fa2
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-26 13:10:42 +0300

    feat: Add tap functionality to list tile

--- lib/widgets/product_list_tile.dart
+++ lib/widgets/product_list_tile.dart
@@ -1,10 +1,10 @@
 import 'package:devop/models/products/product.dart';
 import 'package:flutter/material.dart';
-import 'package:your_app_name/models/products/product.dart';
 
 class ProductListTile extends StatefulWidget {
   final Product product;
-  final VoidCallback;
+  //feat: Add tap functionality to list tile
+  final VoidCallback onTap;
 
 
   const ProductListTile ({
