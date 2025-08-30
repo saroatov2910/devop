4536b2debf3c5dfc97fb886259ef294a089ad62e
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-08-30 19:58:59 +0300

    Initialize Firestore user collection reference.

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,21 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserService {
-  static final CollectionReference usersCollection = FirebaseFirestore.instance
-      .collection('users');
-
-  static Future<void> createUserProfile({
-    required String uid,
-    required String username,
-    required String email,
-    required String phoneNumber,
-  }) async {
-    await usersCollection.doc(uid).set({
-      'uid': uid,
-      'username': username,
-      'email': email,
-      'phoneNumber': phoneNumber,
-      'createdAt': FieldValue.serverTimestamp(),
-    });
-  }
+  static final CollectionReference userCollection = FirebaseFirestore.instance
+      .collection('user');
 }
+
+// class UserService {
+//   static final CollectionReference usersCollection = FirebaseFirestore.instance
+//       .collection('users');
+
+//   static Future<void> createUserProfile({
+//     required String uid,
+//     required String username,
+//     required String email,
+//     required String phoneNumber,
+//   }) async {
+//     await usersCollection.doc(uid).set({
+//       'uid': uid,
+//       'username': username,
+//       'email': email,
+//       'phoneNumber': phoneNumber,
+//       'createdAt': FieldValue.serverTimestamp(),
+//     });
+//   }
+// }
