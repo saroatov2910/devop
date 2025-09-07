c90b0409c4a2b0dccc458fc9a579225acac5a773
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-09-07 11:16:00 +0300

    Add Notification class

---
+++ lib/models/Notification/otification.dart
@@ -0,0 +1,11 @@
+class AppNotification {
+  final String title;
+  final String body;
+  final String message;
+
+  AppNotification({
+    required this.title,
+    required this.body,
+    required this.message,
+  });
+}
