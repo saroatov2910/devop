9e7eee7d0f6de7632bc361b80b0a7b786d6da81b
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-09-08 12:31:04 +0300

    display only expiration date in product list tile subtitle

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,3 +1,30 @@
+import '../models/products/product.dart';
+import '../models/Notification/otification.dart';
+
 class NotificationService {
-  // Method to show a notification
+  static List<AppNotification> getExpiringProductNotifications(
+    List<Product> products, {
+    int days = 3,
+  }) {
+    final now = DateTime.now();
+    return products
+        .where((product) {
+          final daysSinceExpired = now
+              .difference(product.expirationDate)
+              .inDays;
+          return product.expirationDate.isBefore(now) &&
+              daysSinceExpired <= days &&
+              daysSinceExpired >= 0;
+        })
+        .map(
+          (product) => AppNotification(
+            title: "Expiration Alert",
+            body:
+                "המוצר '${product.name}' עומד לפוג תוקף בעוד ${product.expirationDate.difference(now).inDays} ימים.",
+            message:
+                "תוקף עד: ${product.expirationDate.toLocal().toString().split(' ')[0]}",
+          ),
+        )
+        .toList();
+  }
 }
