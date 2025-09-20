fe2a948320d75c692f542aa922b9616080f05f2d
saroatov2910 <98228992+saroatov2910@users.noreply.github.com>
2025-09-21 01:04:02 +0300

    feat: add ProductListTile widget for displaying product details with checkbox and quantity

--- lib/widgets/product_list_tile.dart
+++ lib/widgets/product_list_tile.dart
@@ -1,40 +1,32 @@
-// lib/widgets/product_list_tile.dart
 import 'package:flutter/material.dart';
 import '../models/products/product.dart';
 
+// Widget that displays a product in a list tile
 class ProductListTile extends StatelessWidget {
-  final Product product;
-  final VoidCallback onTap;
+  final Product product; // The product to display
+  final VoidCallback onTap; // Callback when tile or checkbox is tapped
+  final int count; // The number of products with the same barcode
 
   const ProductListTile({
     super.key,
     required this.product,
     required this.onTap,
+    required this.count,
   });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Icon(product.icon),
-      title: Text(product.name),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(product.description),
-          Text("ID: ${product.id}"),
-          Text("Barcode: ${product.barcode}"), // barcode
-          Text(
-            "Expires:${product.expirationDate.toLocal().toString().split(' ')[0]}",
-          ),
-        ],
+      leading: Icon(product.icon), // Product icon
+      title: Text(product.name), // Product name
+      subtitle: Text(
+        "תוקף עד: ${product.expirationDate.toLocal().toString().split(' ')[0]}\nכמות: ${product.quantity}",
       ),
       trailing: Checkbox(
-        value: product.checked,
-        onChanged: (bool? value) {
-          onTap();
-        },
+        value: product.checked, // Checked state
+        onChanged: (_) => onTap(), // Calls onTap when checkbox changes
       ),
-      onTap: onTap,
+      onTap: onTap, // Calls onTap when tile is tapped
     );
   }
 }
